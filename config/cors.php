f5f313b1faaaa590f1595917b22746c134be8fd9
tyleropo <tylermail010@gmail.com>
2025-12-26 20:27:33 +0800

    chore: Allow local network access by adding an IP to CORS origins and binding the dev server to all interfaces.

--- config/cors.php
+++ config/cors.php
@@ -22,6 +22,7 @@ return [
     'allowed_origins' => [
         'http://localhost:3000',
         'http://127.0.0.1:3000',
+        'http://192.168.254.114:3000',
     ],
 
     'allowed_origins_patterns' => [],
